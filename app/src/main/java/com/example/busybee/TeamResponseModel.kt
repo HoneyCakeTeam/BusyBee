ab0d7a19f584245fd7ec68cbdbd13aa86adb91e3
Aziza <aziza.helmy35@gmail.com>
2023-04-06 21:29:02 +0200

    add response model for team todo

---
+++ app/src/main/java/com/example/busybee/TeamResponseModel.kt
@@ -0,0 +1,24 @@
+package com.example.busybee
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Created by Aziza Helmy on 4/6/2023.
+ */
+@Parcelize
+data class TeamResponseModel(
+    val value: List<Value>,
+    val message: String?,
+    val isSuccess: Boolean
+) : Parcelable
+
+@Parcelize
+data class Value(
+    val id: String,
+    val title: String,
+    val description: String,
+    val assignee: String,
+    val status: Int,
+    val creationTime: String
+) : Parcelable
