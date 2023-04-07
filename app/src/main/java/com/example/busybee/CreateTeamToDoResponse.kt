66f8b827164e1d562ad3b23a7f2893bd5f556504
Hagar-Gomaa <hagargomaa313@gmail.com>
2023-04-08 00:35:44 +0200

    update class create team response name

--- app/src/main/java/com/example/busybee/CreateTeamToDoResponse.kt
+++ app/src/main/java/com/example/busybee/CreateTeamToDoResponse.kt
@@ -1,6 +1,6 @@
 package com.example.busybee
 
-data class TeamResponseCreateToDoModel(
+data class CreateTeamToDoResponse(
     val value: Value,
     val message :String?,
     val isSuccessful :String
