9c84eb142c84dd5463163b3a510c70ecf5808300
Aziza <aziza.helmy35@gmail.com>
2023-04-07 02:52:20 +0200

    add basics screen

---
+++ app/src/main/java/com/example/busybee/data/source/ConnectionBuilder.kt
@@ -0,0 +1,7 @@
+package com.example.busybee.data.source
+
+/**
+ * Created by Aziza Helmy on 4/7/2023.
+ */
+class ConnectionBuilder {
+}
---
+++ app/src/main/java/com/example/busybee/ui/login/presenter/LoginPresenter.kt
@@ -0,0 +1,7 @@
+package com.example.busybee.ui.login.presenter
+
+/**
+ * Created by Aziza Helmy on 4/7/2023.
+ */
+class LoginPresenter {
+}
