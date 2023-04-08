1e899d860bb0d4f81be0d5c7e8e2b613cca9471e
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-08 21:17:51 +0200

    replace !! by ?

--- app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
@@ -8,7 +8,7 @@ object SharedPreferencesUtils {
     private const val SHARED_PREFERENCES_NAME = "MySharedPreferences"
     private const val USER_TOKEN = "keyToken"
 
-    fun initPreferencesUtil(context: Context) {
+    private fun initPreferencesUtil(context: Context) {
         sharedPreferences = context.getSharedPreferences(
             SHARED_PREFERENCES_NAME,
             Context.MODE_PRIVATE
@@ -16,8 +16,8 @@ object SharedPreferencesUtils {
     }
 
     var token: String?
-        get() = sharedPreferences!!.getString(USER_TOKEN, null)
+        get() = sharedPreferences?.getString(USER_TOKEN, null)
         set(token) {
-            sharedPreferences!!.edit().putString(USER_TOKEN, token).apply()
+            sharedPreferences?.edit()?.putString(USER_TOKEN, token)?.apply()
         }
 }
