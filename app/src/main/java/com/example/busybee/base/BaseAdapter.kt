61b8e578af38517b970608d5e8c38828d0a2ba10
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-08 02:49:49 +0200

    create baseAdapter

---
+++ app/src/main/java/com/example/busybee/base/BaseAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.busybee.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseAdapter<T, VB : ViewBinding>(
+    private  var items: List<T>
+): RecyclerView.Adapter<BaseAdapter.BaseViewHolder<VB>>(){
+
+    abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<VB> {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = bindingInflater(inflater, parent, false)
+        return BaseViewHolder(binding)
+    }
+
+
+    class BaseViewHolder<VB : ViewBinding>(val binding: VB):RecyclerView.ViewHolder(binding.root)
+
+
+
+
+}
