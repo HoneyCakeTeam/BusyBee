82413eb625a704fd49a27eb46f70a04915046534
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-08 21:10:57 +0200

    update token name

--- app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
@@ -6,7 +6,7 @@ import android.content.SharedPreferences
 object SharedPreferencesUtils {
     private var sharedPreferences: SharedPreferences? = null
     private const val SHARED_PREFERENCES_NAME = "MySharedPreferences"
-    private const val KEY_TOKEN = "keyToken"
+    private const val USER_TOKEN = "keyToken"
 
     fun initPreferencesUtil(context: Context) {
         sharedPreferences = context.getSharedPreferences(
@@ -16,8 +16,8 @@ object SharedPreferencesUtils {
     }
 
     var token: String?
-        get() = sharedPreferences!!.getString(KEY_TOKEN, null)
+        get() = sharedPreferences!!.getString(USER_TOKEN, null)
         set(token) {
-            sharedPreferences!!.edit().putString(KEY_TOKEN, token).apply()
+            sharedPreferences!!.edit().putString(USER_TOKEN, token).apply()
         }
 }
