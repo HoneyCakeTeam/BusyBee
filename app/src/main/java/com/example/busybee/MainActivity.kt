63a8b230737c4a2b050d52d9b9dd6d2dd2dc8dda
Aziza <aziza.helmy35@gmail.com>
2023-04-08 22:46:17 +0200

    Revert "make status bar transparent in light and dark theme" This reverts commit 749c1751

--- app/src/main/java/com/example/busybee/MainActivity.kt
+++ app/src/main/java/com/example/busybee/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.example.busybee
 
+import android.content.res.Configuration
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.busybee.data.models.LoginRequest
-import com.example.busybee.data.models.LoginResponse
-import com.example.busybee.data.source.ConnectionBuilder.Companion.logInterceptor
-import com.example.busybee.data.source.executeWithCallbacks
-import com.example.busybee.utils.Constant.loginUrl
-import com.google.gson.reflect.TypeToken
-import okhttp3.OkHttpClient
-import okhttp3.Request
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        statusBarTheme()
+    }
 
+    @Suppress("DEPRECATION")
+    private fun statusBarTheme() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
+
+        if (isDarkTheme()) {
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        } else {
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val decorView = window.decorView
+            decorView.systemUiVisibility =
+                decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+    }
+
+    private fun isDarkTheme(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
