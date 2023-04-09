172685d701451197aea5fec6cadd7bd8eab9f2e5
Nour Elden Alaa <nourelden515@gmail.com>
2023-04-09 02:16:53 +0200

    fix LoginFragment.kt

--- app/src/main/java/com/example/busybee/ui/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/example/busybee/ui/login/presenter/LoginPresenter.kt
@@ -7,9 +7,9 @@ import com.example.busybee.ui.login.view.LoginViewInterface
 class LoginPresenter(val loginViewInterface : LoginViewInterface, val repository : Repository) : LoginPresenterInterface {
     override fun <T> logIn(userName: String, password: String, onSuccessCallback: (response: T) -> Unit ,
                            onFailureCallback: (error: Throwable) -> Unit ){
-        repository.logIn<T>(userName , password , onSuccessCallback , onFailureCallback)
+        repository.logIn(userName , password , onSuccessCallback , onFailureCallback)
 
-        loginViewInterface.logIn<T>(userName,password , onSuccessCallback, onFailureCallback)
+        loginViewInterface.logIn(userName,password , onSuccessCallback, onFailureCallback)
     }
 
 }
--- app/src/main/java/com/example/busybee/ui/login/view/LoginFragment.kt
+++ app/src/main/java/com/example/busybee/ui/login/view/LoginFragment.kt
@@ -1,12 +1,6 @@
 package com.example.busybee.ui.login.view
 
-import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.busybee.R
+import android.widget.Toast
 import com.example.busybee.base.BaseFragment
 import com.example.busybee.data.Repository
 import com.example.busybee.data.models.LoginResponse
@@ -15,9 +9,9 @@ import com.example.busybee.ui.login.presenter.LoginPresenter
 import com.example.busybee.ui.login.presenter.LoginPresenterInterface
 
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>() , LoginViewInterface{
+class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginViewInterface {
 
-    private val presenter: LoginPresenterInterface by lazy { LoginPresenter(this , Repository()) }
+    private val presenter: LoginPresenterInterface by lazy { LoginPresenter(this, Repository()) }
     override val TAG: String = this::class.simpleName.toString()
 
     override fun getViewBinding(): FragmentLoginBinding =
@@ -25,10 +19,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() , LoginViewInterface{
 
     override fun setUp() {
 
-        logIn("nourelden515" , "123456789")
+        logIn("nourelden515", "123456789")
     }
 
-    private fun logIn(userName: String, password: String) {
+    /*private fun logIn(userName: String, password: String) {
         presenter.logIn<LoginResponse>(
             userName,
             password,
@@ -43,7 +37,42 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() , LoginViewInterface{
         onFailureCallback: (error: Throwable) -> Unit
     ) {
 
+    }*/
+
+    private fun logIn(userName: String, password: String) {
+        // Call the Presenter's login method with the provided username and password
+        presenter.logIn<LoginResponse>(
+            userName,
+            password,
+            onSuccessCallback = { response ->
+                // Handle the successful login response here
+                showLoginSuccessMessage(response)
+            },
+            onFailureCallback = { error ->
+                // Handle the login failure here
+                showLoginErrorMessage(error)
+            }
+        )
     }
 
+    override fun <T> logIn(
+        userName: String,
+        password: String,
+        onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit
+    ) {
+        // This method will be called by the Presenter when it receives the login response
+        // You can ignore this for now
+    }
 
+    private fun showLoginSuccessMessage(response: LoginResponse) {
+        // Display a success message to the user
+        Toast.makeText(requireContext(), "Login successful!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showLoginErrorMessage(error: Throwable) {
+        // Display an error message to the user
+        Toast.makeText(requireContext(), "Login failed: ${error.message}", Toast.LENGTH_SHORT)
+            .show()
+    }
 }
