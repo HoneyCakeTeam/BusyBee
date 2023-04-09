62d62202ac9821a9b67665a367b072b6b8dd8ab6
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-09 15:51:04 +0200

    implement Home Fragment

--- app/src/main/java/com/example/busybee/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/view/HomeFragment.kt
@@ -6,17 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.busybee.R
+import com.example.busybee.base.BaseFragment
+import com.example.busybee.databinding.FragmentHomeBinding
+import com.example.busybee.ui.home.teamTask.done.TeamDoneFragment
+import com.example.busybee.ui.home.teamTask.inProgress.TeamInProgressFragment
+import com.example.busybee.ui.home.teamTask.toDo.TeamToDoFragment
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    private lateinit var fragmentList: List<Fragment>
+    private val teamToDoFragment = TeamToDoFragment()
+    private val teamDoneFragment = TeamDoneFragment()
+    private val teamInProgressFragment = TeamInProgressFragment()
+    override val TAG = "HomeFragment"
+
+    override fun getViewBinding(): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(layoutInflater)
+    }
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun setUp() {
+        fragmentList = listOf(teamToDoFragment ,teamInProgressFragment,teamDoneFragment)
     }
 
 
