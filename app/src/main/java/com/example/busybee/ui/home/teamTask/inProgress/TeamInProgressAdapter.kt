dedb3326af4c6a668a7095d16060e7f488c1cdd4
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-09 15:49:05 +0200

    create recycler in inProress fragment and adapter

---
+++ app/src/main/java/com/example/busybee/ui/home/teamTask/inProgress/TeamInProgressAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.busybee.ui.home.teamTask.inProgress
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.busybee.base.BaseAdapter
+import com.example.busybee.data.models.TeamToDo
+import com.example.busybee.databinding.TaskItemBinding
+
+class TeamInProgressAdapter(private var teamInProgressList: List<TeamToDo>) :
+    BaseAdapter<TeamToDo, TaskItemBinding>(teamInProgressList) {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> TaskItemBinding
+        get() = TaskItemBinding::inflate
+
+    override fun onBindViewHolder(
+        holder: BaseViewHolder<TaskItemBinding>,
+        position: Int,
+        currentItem: TeamToDo,
+    ) {
+        holder.binding.apply {
+            textTaskName.text = currentItem.title
+            textTaskDescription.text = currentItem.description
+            textTaskTime.text = currentItem.creationTime
+        }
+
+    }
+}
