3cd6ba60afebf423665158f32b5f9a92edbb2e0d
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-09 22:18:03 +0200

    add listener for fab button in personal tasks

--- app/src/main/java/com/example/busybee/ui/createTask/CreateNewTaskFragment.kt
+++ app/src/main/java/com/example/busybee/ui/createTask/CreateNewTaskFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.example.busybee.databinding.BottomSheetCreateTaskBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class CreateTask:BottomSheetDialogFragment() {
+class CreateNewTaskFragment:BottomSheetDialogFragment() {
+    private lateinit var binding:BottomSheetCreateTaskBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View{
+        binding = BottomSheetCreateTaskBinding.inflate(layoutInflater,container,false )
+        return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/busybee/ui/home/personalTask/toDoTask/PersonalToDoFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/personalTask/toDoTask/PersonalToDoFragment.kt
@@ -3,17 +3,30 @@ package com.example.busybee.ui.home.personalTask.toDoTask
 
 import com.example.busybee.base.BaseFragment
 import com.example.busybee.databinding.FragmentPersonalToDoBinding
+import com.example.busybee.ui.createTask.CreateNewTaskFragment
 
-class PersonalToDoFragment (): BaseFragment<FragmentPersonalToDoBinding>() {
-        private lateinit var adapter: PersonalToDoAdapter
-        override val TAG = "ToDoFragment"
+class PersonalToDoFragment() : BaseFragment<FragmentPersonalToDoBinding>() {
+    private lateinit var adapter: PersonalToDoAdapter
+    private val createNewTaskFragment = CreateNewTaskFragment()
+    override val TAG = "ToDoFragment"
 
-        override fun getViewBinding(): FragmentPersonalToDoBinding {
-            return FragmentPersonalToDoBinding.inflate(layoutInflater)
-        }
+    override fun getViewBinding(): FragmentPersonalToDoBinding {
+        return FragmentPersonalToDoBinding.inflate(layoutInflater)
+    }
 
-        override fun setUp() {
-            adapter = PersonalToDoAdapter(emptyList())
-            binding.recyclerToDo.adapter = adapter
+    override fun setUp() {
+        adapter = PersonalToDoAdapter(emptyList())
+        binding.recyclerToDo.adapter = adapter
+
+        addCallBacks()
+    }
+
+    private fun addCallBacks() {
+        binding.buttonAddNewPersonalTask.setOnClickListener {
+            createNewTaskFragment.show(
+                requireActivity().supportFragmentManager,
+                createNewTaskFragment.tag
+            )
         }
     }
+}
