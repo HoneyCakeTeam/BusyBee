b62a4ab21a784fa7970837caedb95b8e4a0932d5
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-10 12:47:10 +0200

    add expirationDate variable

--- app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
@@ -7,6 +7,7 @@ object SharedPreferencesUtils {
     private var sharedPreferences: SharedPreferences? = null
     private const val SHARED_PREFERENCES_NAME = "MySharedPreferences"
     private const val USER_TOKEN = "keyToken"
+    private const val EXPIRATION_DATE_KEY = "expirationDate"
 
     fun initPreferencesUtil(context: Context) {
         sharedPreferences = context.getSharedPreferences(
@@ -20,6 +21,12 @@ object SharedPreferencesUtils {
         set(value) {
             token ?: clearToken()
             sharedPreferences?.edit()?.putString(USER_TOKEN, value)?.apply()
+        }
+
+    var expirationDate: Long?
+        get() = sharedPreferences?.getLong(EXPIRATION_DATE_KEY, Long.MIN_VALUE)
+        set(value) {
+            sharedPreferences?.edit()?.putLong(EXPIRATION_DATE_KEY, value!!)?.apply()
         }
 
     private fun clearToken() = sharedPreferences?.edit()?.remove(USER_TOKEN)?.apply()
