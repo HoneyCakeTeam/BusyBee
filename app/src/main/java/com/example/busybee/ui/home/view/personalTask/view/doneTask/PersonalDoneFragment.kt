03a61a59329cb41a5dd5732bf925de1c10f57515
Mohamed Naser <mahmmedn19@gmail.com>
2023-04-11 00:20:04 +0200

    implement methods from personal task view presenter

--- app/src/main/java/com/example/busybee/ui/home/view/personalTask/view/doneTask/PersonalDoneFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/view/personalTask/view/doneTask/PersonalDoneFragment.kt
@@ -1,19 +1,44 @@
 package com.example.busybee.ui.home.view.personalTask.view.doneTask
 
 import com.example.busybee.base.BaseFragment
+import com.example.busybee.data.Repository
+import com.example.busybee.data.models.PersonalGetToDoListResponse
 import com.example.busybee.databinding.FragmentDonePersonalBinding
+import com.example.busybee.ui.home.view.personalTask.view.PersonalTasksViewPresenter
+import com.example.busybee.ui.home.view.personalTask.view.presenter.PersonalTasksPresenter
+import com.example.busybee.ui.home.view.personalTask.view.presenter.PersonalTasksPresenterInterface
 
 
-class PersonalDoneFragment : BaseFragment<FragmentDonePersonalBinding>() {
-    private lateinit var adapter : DoneAdapter
+class PersonalDoneFragment : BaseFragment<FragmentDonePersonalBinding>() ,PersonalTasksViewPresenter{
+    private lateinit var adapter: DoneAdapter
     override val TAG = this::class.java.simpleName.toString()
+
+    private val presenter: PersonalTasksPresenterInterface by lazy {
+        PersonalTasksPresenter(
+            Repository(requireContext())
+        )
+    }
+
     override fun getViewBinding(): FragmentDonePersonalBinding {
         return FragmentDonePersonalBinding.inflate(layoutInflater)
     }
 
     override fun setUp() {
-        adapter= DoneAdapter(emptyList())
+        getDoneTasks()
+        adapter = DoneAdapter(emptyList())
         binding.recyclerDone.adapter = adapter
+    }
+
+    private fun getDoneTasks() {
+        presenter.getPersonalTasks(::onSuccessResponse,::onFailureResponse)
+    }
+
+    override fun onSuccessResponse(response: PersonalGetToDoListResponse) {
+
+    }
+
+    override fun onFailureResponse(error: Throwable) {
+
     }
 
 }
