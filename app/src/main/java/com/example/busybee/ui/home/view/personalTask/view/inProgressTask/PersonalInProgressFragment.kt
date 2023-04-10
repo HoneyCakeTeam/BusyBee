7c049f287a2bdd26144bf2b2359adce2f1f4e4cb
Nahla <nahla@email.com>
2023-04-11 00:48:50 +0200

    get in progress tasks

--- app/src/main/java/com/example/busybee/ui/home/view/personalTask/view/inProgressTask/PersonalInProgressFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/view/personalTask/view/inProgressTask/PersonalInProgressFragment.kt
@@ -1,18 +1,63 @@
 package com.example.busybee.ui.home.view.personalTask.view.inProgressTask
 
+import com.example.busybee.R
 import com.example.busybee.base.BaseFragment
+import com.example.busybee.data.Repository
+import com.example.busybee.data.models.PersonalGetToDoListResponse
+import com.example.busybee.data.models.PersonalTodo
 import com.example.busybee.databinding.FragmentPersonalInProgressBinding
-import com.example.busybee.ui.home.view.personalTask.view.inProgressTask.PersonalInProgressAdapter
+import com.example.busybee.ui.home.view.personalTask.view.PersonalTasksViewPresenter
+import com.example.busybee.ui.home.view.personalTask.view.presenter.PersonalTasksPresenter
+import com.example.busybee.ui.home.view.personalTask.view.presenter.PersonalTasksPresenterInterface
 
-class PersonalInProgressFragment : BaseFragment<FragmentPersonalInProgressBinding>() {
-    private lateinit var adapter : PersonalInProgressAdapter
+
+class PersonalInProgressFragment : BaseFragment<FragmentPersonalInProgressBinding>(),
+PersonalTasksViewPresenter {
+    private lateinit var adapter: PersonalInProgressAdapter
     override val TAG = this::class.java.simpleName.toString()
+
+    private val presenter: PersonalTasksPresenterInterface by lazy {
+        PersonalTasksPresenter(
+            Repository(requireContext())
+        )
+    }
+
     override fun getViewBinding(): FragmentPersonalInProgressBinding {
         return FragmentPersonalInProgressBinding.inflate(layoutInflater)
     }
 
     override fun setUp() {
-        adapter = PersonalInProgressAdapter(emptyList())
+        getToDoTasks()
+    }
+
+    private fun getToDoTasks() {
+        presenter.getPersonalTasks(::onSuccessResponse, ::onFailureResponse)
+    }
+
+    override fun onSuccessResponse(response: PersonalGetToDoListResponse) {
+        requireActivity().runOnUiThread {
+            val personalToDoTasks = getPersonalInProgressTasks(response.value)
+            initiateAdapter(personalToDoTasks)
+            setHeader(personalToDoTasks)
+        }
+    }
+
+    private fun setHeader(personalInProgressTasks: List<PersonalTodo>) {
+        binding.headerInProgress.taskCount.text =
+            getString(R.string.tasks, personalInProgressTasks.size)
+        binding.headerInProgress.todoSection.setBackgroundResource(R.drawable.shape_section_orange)
+
+    }
+    private fun getPersonalInProgressTasks(personaTasksList: List<PersonalTodo>): List<PersonalTodo>
+        = personaTasksList.filter { it.status == 1 }
+
+
+    private fun initiateAdapter(personalTasksList: List<PersonalTodo>) {
+        adapter = PersonalInProgressAdapter(personalTasksList)
         binding.recyclerInProgress.adapter = adapter
+    }
+
+    override fun onFailureResponse(error: Throwable) {
+        log("Failed: ${error.message}")
     }
 }
