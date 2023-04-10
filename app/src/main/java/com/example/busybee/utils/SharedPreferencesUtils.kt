ca7ab4470568d3f8c6ec1e25aa44d9ecb8c8ae7d
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-10 12:46:07 +0200

    add clearToken() function

--- app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
+++ app/src/main/java/com/example/busybee/utils/SharedPreferencesUtils.kt
@@ -8,7 +8,7 @@ object SharedPreferencesUtils {
     private const val SHARED_PREFERENCES_NAME = "MySharedPreferences"
     private const val USER_TOKEN = "keyToken"
 
-    private fun initPreferencesUtil(context: Context) {
+    fun initPreferencesUtil(context: Context) {
         sharedPreferences = context.getSharedPreferences(
             SHARED_PREFERENCES_NAME,
             Context.MODE_PRIVATE
@@ -17,7 +17,11 @@ object SharedPreferencesUtils {
 
     var token: String?
         get() = sharedPreferences?.getString(USER_TOKEN, null)
-        set(token) {
-            sharedPreferences?.edit()?.putString(USER_TOKEN, token)?.apply()
+        set(value) {
+            token ?: clearToken()
+            sharedPreferences?.edit()?.putString(USER_TOKEN, value)?.apply()
         }
+
+    private fun clearToken() = sharedPreferences?.edit()?.remove(USER_TOKEN)?.apply()
+
 }
