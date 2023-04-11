530e41a5705f9904e486e4ac4fe0e46af73412a0
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-11 02:11:49 +0200

    add constant

--- app/src/main/java/com/example/busybee/utils/Constant.kt
+++ app/src/main/java/com/example/busybee/utils/Constant.kt
@@ -4,8 +4,10 @@ package com.example.busybee.utils
  * Created by Aziza Helmy on 4/8/2023.
  */
 object Constant {
-     const val baseUrl = "https://team-todo-62dmq.ondigitalocean.app"
-     const val loginUrl = "$baseUrl/login"
-     const val UPDATE_STATUS_END_POINT = "$baseUrl/todo/team"
+     const val BASE_URL = "https://team-todo-62dmq.ondigitalocean.app"
+     const val loginUrl = "$BASE_URL/login"
+     const val UPDATE_STATUS_END_POINT = "$BASE_URL/todo/team"
+     const val PERSONAL_TASKS_END_POINT = "/todo/personal"
+
 
 }
