60b2b8fa26c3f0fd9f02527e7c480f79f5b2a215
hamsa <hamsaalsaadie@gmail.com>
2023-04-11 03:33:41 +0300

    add DetailsPresenterInterface.kt

--- app/src/main/java/com/example/busybee/ui/details/view/DetailsViewInterface.kt
+++ app/src/main/java/com/example/busybee/ui/details/view/DetailsViewInterface.kt
@@ -2,9 +2,11 @@ package com.example.busybee.ui.details.view
 
 import com.example.busybee.data.models.LoginResponse
 import com.example.busybee.data.models.PersonalUpdateStatusResponse
+import com.example.busybee.data.models.TeamUpdateStatusResponse
 
 interface DetailsViewInterface {
     fun updateTasksPersonalStatus (id :String , status :Int)
     fun onSuccessPersonalResponse(response: PersonalUpdateStatusResponse)
     fun onFailureResponse(error: Throwable)
+    fun onSuccessTeamResponse(response: TeamUpdateStatusResponse)
 }
