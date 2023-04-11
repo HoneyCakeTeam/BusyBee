fc6ac37e4dbaae14a9f14e2360c16c63f9370ca9
hamsa <hamsaalsaadie@gmail.com>
2023-04-11 03:35:36 +0300

    fix code

--- app/src/main/java/com/example/busybee/ui/details/presenter/DetailsPresenter.kt
+++ app/src/main/java/com/example/busybee/ui/details/presenter/DetailsPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.busybee.ui.details.presenter
 
+import com.example.busybee.data.Repository
 import com.example.busybee.data.RepositoryInterface
 import com.example.busybee.ui.details.view.DetailsViewInterface
 
@@ -16,6 +17,15 @@ class DetailsPresenter(
     ) {
         repository.updateTasksPersonalStatus(id, status, onSuccessCallback, onFailureCallback)
 
+    }
+
+    override fun <T> updateTasksTeamStatus(
+        idTask: String,
+        status: Int,
+        onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit
+    ) {
+        repository.updateTasksTeamStatus(idTask, status, onSuccessCallback, onFailureCallback)
     }
 
 }
