c67fa2aabdbc3fccbfdbf96630011b9831331f00
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-04-12 01:13:58 +0200

    get response from PersonalToDoListResponse instead of PersonalCreateToDoResponse

--- app/src/main/java/com/example/busybee/ui/home/personaltask/view/todo/PersonalToDoViewInterface.kt
+++ app/src/main/java/com/example/busybee/ui/home/personaltask/view/todo/PersonalToDoViewInterface.kt
@@ -1,8 +1,9 @@
 package com.example.busybee.ui.home.personaltask.view.todo
 import com.example.busybee.data.models.PersonalCreateToDoResponse
+import com.example.busybee.data.models.PersonalToDoListResponse
 
 interface PersonalToDoViewInterface {
     fun personalCreateToDo(title: String, description: String)
-    fun onSuccessResponse(response: PersonalCreateToDoResponse)
+    fun onSuccessResponse(response: PersonalToDoListResponse)
     fun onFailureResponse(error: Throwable)
 }
--- app/src/main/java/com/example/busybee/ui/home/teamtask/presenter/TeamPresenter.kt
+++ app/src/main/java/com/example/busybee/ui/home/teamtask/presenter/TeamPresenter.kt
@@ -2,7 +2,8 @@ package com.example.busybee.ui.home.teamtask.presenter
 
 import com.example.busybee.data.RepositoryInterface
 
-class TeamPresenter(private val repository: RepositoryInterface) : TeamPresenterInterface {
+class TeamPresenter(private val repository: RepositoryInterface)
+    : TeamPresenterInterface {
     override fun <T> getAllTeamTasks(
         onSuccessCallback: (response: T) -> Unit,
         onFailureCallback: (error: Throwable) -> Unit
