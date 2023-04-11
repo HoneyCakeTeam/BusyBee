ebdafd6f892a2a644968ec8fc0128001d50a1634
Hagar-Gomaa <hagargomaa313@gmail.com>
2023-04-11 18:28:57 +0200

    toggle button

--- app/src/main/java/com/example/busybee/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/busybee/ui/setting/SettingFragment.kt
@@ -4,15 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.busybee.R
+import com.example.busybee.databinding.FragmentSettingsBinding
 
 class SettingFragment : Fragment() {
+    lateinit var binding: FragmentSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.switchTheme.isChecked= lightState
+        callBacks()
+    }
+
+
+    private fun callBacks() {
+        binding.switchTheme.setOnCheckedChangeListener { _, _ ->
+            if (isCurrentUiDarkTheme())
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            lightState=!lightState
+        }
+
+    }
+
+    private fun isCurrentUiDarkTheme() =
+        AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+
+    companion object {
+        var lightState = true
     }
 }
