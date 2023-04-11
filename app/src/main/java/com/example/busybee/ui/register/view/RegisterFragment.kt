ec8cfa769e253ba15e43839311f4679bd3ed6a47
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-04-11 13:50:14 +0200

    feat: implement fragment code

--- app/src/main/java/com/example/busybee/ui/register/view/RegisterFragment.kt
+++ app/src/main/java/com/example/busybee/ui/register/view/RegisterFragment.kt
@@ -1,21 +1,68 @@
 package com.example.busybee.ui.register.view
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.busybee.R
+import android.widget.Toast
+import com.example.busybee.base.BaseFragment
+import com.example.busybee.data.Repository
+import com.example.busybee.data.models.SignUpResponse
+import com.example.busybee.databinding.FragmentRegisterBinding
+import com.example.busybee.ui.register.presenter.RegisterPresenter
+import com.example.busybee.ui.register.presenter.RegisterPresenterInterface
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(), RegisterViewInterface {
+
+    private val presenter: RegisterPresenterInterface by lazy {
+        RegisterPresenter(
+            this,
+            Repository(requireContext())
+        )
+    }
+    override val TAG: String = this::class.simpleName.toString()
+
+    override fun getViewBinding(): FragmentRegisterBinding =
+        FragmentRegisterBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+        binding.buttonSignUp.setOnClickListener {
+            val userName = binding.textFieldName.editText?.text.toString()
+            val password = binding.textFieldPassword.editText?.text.toString()
+            val confirmPassword = binding.textFieldConfirmPassword.editText?.text.toString()
+            signUp(userName, password)
+        }
+    }
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    override fun signUp(userName: String, password: String) {
+        presenter.signUp<SignUpResponse>(
+            userName,
+            password,
+            onSuccessCallback = { response ->
+                onSuccessResponse(response)
+            },
+            onFailureCallback = { error ->
+                onFailureResponse(error)
+            }
+        )
+    }
+
+    override fun onSuccessResponse(response: SignUpResponse) {
+        // here we will update the ui
+        activity?.runOnUiThread {
+            Toast.makeText(
+                requireContext(),
+                "sign up successful",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onFailureResponse(error: Throwable) {
+        activity?.runOnUiThread {
+            Toast.makeText(
+                requireContext(),
+                "${error.message} ",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 
