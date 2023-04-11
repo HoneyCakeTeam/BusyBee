e245d8f965a1872808ba5477f726c520772f355a
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-11 02:41:30 +0200

    implementation Details Fragment

--- app/src/main/java/com/example/busybee/ui/details/view/DetailsFragment.kt
+++ app/src/main/java/com/example/busybee/ui/details/view/DetailsFragment.kt
@@ -1,48 +1,63 @@
 package com.example.busybee.ui.details.view
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.busybee.R
+import android.widget.Toast
 import com.example.busybee.base.BaseFragment
+import com.example.busybee.data.Repository
 import com.example.busybee.data.models.PersonalUpdateStatusResponse
+import com.example.busybee.data.models.TeamUpdateStatusResponse
 import com.example.busybee.databinding.FragmentDetailsBinding
-import com.example.busybee.databinding.FragmentLoginBinding
-import com.example.busybee.ui.login.view.LoginViewInterface
+import com.example.busybee.ui.details.presenter.DetailsPresenter
+import com.example.busybee.ui.details.presenter.DetailsPresenterInterface
 
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), DetailsViewInterface {
-    override val TAG: String
-        get() = TODO("Not yet implemented")
-
-    override fun getViewBinding(): FragmentDetailsBinding {
-        TODO("Not yet implemented")
+    override val TAG = this::class.java.simpleName.toString()
+    private val presenter: DetailsPresenterInterface by lazy {
+        DetailsPresenter(
+            this, Repository(requireContext())
+        )
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+
+    override fun getViewBinding(): FragmentDetailsBinding {
+        return FragmentDetailsBinding.inflate(layoutInflater)
     }
 
     override fun setUp() {
         TODO("Not yet implemented")
     }
 
-    override fun updateTasksPersonalStatus(id: String, status: Int) {
-        TODO("Not yet implemented")
+    override fun updateTasksPersonalStatus(idTask: String, status: Int) {
+        presenter.updateTasksPersonalStatus<PersonalUpdateStatusResponse>(idTask,
+            status,
+            onSuccessCallback = { response ->
+                onSuccessPersonalResponse(response)
+            },
+            onFailureCallback = { error ->
+                onFailureResponse(error)
+            })
+
+
     }
 
     override fun onSuccessPersonalResponse(response: PersonalUpdateStatusResponse) {
-        TODO("Not yet implemented")
+        activity?.runOnUiThread {
+            Toast.makeText(
+                requireContext(), "update success! ${response.isSuccess}", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 
     override fun onFailureResponse(error: Throwable) {
+        activity?.runOnUiThread {
+            Toast.makeText(
+                requireContext(), "update fail ! ${error.message} ", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onSuccessTeamResponse(response: TeamUpdateStatusResponse) {
         TODO("Not yet implemented")
     }
 
