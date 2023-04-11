cad95ad1e4b8bcf815f03380f85013e10babbec1
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-11 06:07:15 +0200

    add newInstance function to PersonalInProgressFragment.kt

--- app/src/main/java/com/example/busybee/ui/home/personaltask/view/inProgress/PersonalInProgressFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/personaltask/view/inProgress/PersonalInProgressFragment.kt
@@ -1,10 +1,14 @@
 package com.example.busybee.ui.home.personaltask.view.inProgress
 
+import android.os.Bundle
 import com.example.busybee.base.BaseFragment
 import com.example.busybee.databinding.FragmentPersonalInProgressBinding
+import com.example.busybee.domain.models.PersonalTodos
+import com.example.busybee.ui.home.personaltask.view.done.PersonalDoneFragment
 
 class PersonalInProgressFragment : BaseFragment<FragmentPersonalInProgressBinding>() {
     private lateinit var adapter: PersonalInProgressAdapter
+    private lateinit var done: PersonalTodos
     override val TAG = this::class.java.simpleName.toString()
 
     override fun getViewBinding(): FragmentPersonalInProgressBinding {
@@ -12,8 +16,25 @@ class PersonalInProgressFragment : BaseFragment<FragmentPersonalInProgressBindin
     }
 
     override fun setUp() {
-        adapter = PersonalInProgressAdapter(emptyList())
+        getDons()
+        adapter = PersonalInProgressAdapter(done.values)
         binding.recyclerInProgress.adapter = adapter
+    }
+
+    private fun getDons() {
+        arguments?.let {
+            done = it.getParcelable(PersonalInProgressFragment.PERSONAL_IN_PROGRESS_LIST)!!
+        }
+    }
+
+    companion object {
+        const val PERSONAL_IN_PROGRESS_LIST = "Personal_In_Progress_List"
+        fun newInstance(tasks: PersonalTodos) =
+            PersonalInProgressFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(PERSONAL_IN_PROGRESS_LIST, tasks)
+                }
+            }
     }
 
 }
