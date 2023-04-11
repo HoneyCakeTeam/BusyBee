a97dddb3a6d9db2cc42685136bb34381c51947ab
hamsa <hamsaalsaadie@gmail.com>
2023-04-11 04:07:55 +0300

    refactoring code

--- app/src/main/java/com/example/busybee/ui/details/view/DetailsViewInterface.kt
+++ app/src/main/java/com/example/busybee/ui/details/view/DetailsViewInterface.kt
@@ -3,10 +3,10 @@ package com.example.busybee.ui.details.view
 import com.example.busybee.data.models.PersonalUpdateStatusResponse
 import com.example.busybee.data.models.TeamUpdateStatusResponse
 
-interface DetailsViewInterface {
+interface DetailsViewInterface{
+    fun onSuccessTeamResponse(response: TeamUpdateStatusResponse)
+    fun updateTasksTeamStatus(idTask: String, status: Int)
     fun updateTasksPersonalStatus (idTask :String , status :Int)
     fun onSuccessPersonalResponse(response: PersonalUpdateStatusResponse)
     fun onFailureResponse(error: Throwable)
-    fun onSuccessTeamResponse(response: TeamUpdateStatusResponse)
-    fun updateTasksTeamStatus(idTask: String, status: Int)
 }
