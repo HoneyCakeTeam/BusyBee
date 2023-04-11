78356b764cadb881aa65825ba11d6d06d980b424
Nahla <nahla@email.com>
2023-04-11 16:15:14 +0200

    implement pie chart

--- app/src/main/java/com/example/busybee/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/busybee/ui/setting/SettingFragment.kt
@@ -1,11 +1,18 @@
 package com.example.busybee.ui.setting
 
+import android.graphics.Color
+import com.example.busybee.R
 import com.example.busybee.base.BaseFragment
 import com.example.busybee.databinding.FragmentSettingsBinding
+import org.eazegraph.lib.models.PieModel
 
 class SettingFragment : BaseFragment<FragmentSettingsBinding>() {
     override val TAG: String
         get() = this::class.simpleName.toString()
+
+    private var personalTodos = 30.0f
+    private var personalInProgressTodos = 30.0f
+    private var personalDoneTodos = 40.0f
 
     override fun getViewBinding(): FragmentSettingsBinding =
         FragmentSettingsBinding.inflate(layoutInflater)
@@ -17,7 +24,12 @@ class SettingFragment : BaseFragment<FragmentSettingsBinding>() {
     }
 
     private fun setUpPieChart() {
-        TODO("Not yet implemented")
+        binding.piechart.addPieSlice(PieModel( personalTodos,
+            Color.parseColor(R.color.secondary_500.toString())))
+        binding.piechart.addPieSlice(PieModel( personalInProgressTodos,
+            Color.parseColor(R.color.primary_500.toString())))
+        binding.piechart.addPieSlice(PieModel( personalDoneTodos,
+            Color.parseColor(R.color.color_green.toString())))
     }
 
     private fun addCallBacks() {
