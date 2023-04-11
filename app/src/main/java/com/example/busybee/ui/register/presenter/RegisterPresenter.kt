5893378bace933b948903798c6c05a068e1969c2
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-04-11 13:49:43 +0200

    feat: implement code

--- app/src/main/java/com/example/busybee/ui/register/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/example/busybee/ui/register/presenter/RegisterPresenter.kt
@@ -1,5 +1,19 @@
 package com.example.busybee.ui.register.presenter
 
+import com.example.busybee.data.RepositoryInterface
+import com.example.busybee.ui.register.view.RegisterViewInterface
 
-class RegisterPresenter {
+
+class RegisterPresenter(
+    val registerViewInterface: RegisterViewInterface,
+    private val repository: RepositoryInterface
+) :
+    RegisterPresenterInterface {
+    override fun <T> signUp(
+        userName: String, password: String, onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit
+    ) {
+        repository.signUp(userName, password, onSuccessCallback, onFailureCallback)
+    }
+
 }
