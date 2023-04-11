86f28e25fe787119a322c2110baa133c66d445e1
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-11 02:21:06 +0200

    add update personal status in view details

--- app/src/main/java/com/example/busybee/ui/details/view/DetailsFragment.kt
+++ app/src/main/java/com/example/busybee/ui/details/view/DetailsFragment.kt
@@ -6,9 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.busybee.R
+import com.example.busybee.base.BaseFragment
+import com.example.busybee.data.models.PersonalUpdateStatusResponse
+import com.example.busybee.databinding.FragmentDetailsBinding
+import com.example.busybee.databinding.FragmentLoginBinding
+import com.example.busybee.ui.login.view.LoginViewInterface
 
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>(), DetailsViewInterface {
+    override val TAG: String
+        get() = TODO("Not yet implemented")
+
+    override fun getViewBinding(): FragmentDetailsBinding {
+        TODO("Not yet implemented")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +27,23 @@ class DetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTasksPersonalStatus(id: String, status: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSuccessPersonalResponse(response: PersonalUpdateStatusResponse) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onFailureResponse(error: Throwable) {
+        TODO("Not yet implemented")
     }
 
 
---
+++ app/src/main/java/com/example/busybee/ui/details/view/DetailsViewInterface.kt
@@ -0,0 +1,10 @@
+package com.example.busybee.ui.details.view
+
+import com.example.busybee.data.models.LoginResponse
+import com.example.busybee.data.models.PersonalUpdateStatusResponse
+
+interface DetailsViewInterface {
+    fun updateTasksPersonalStatus (id :String , status :Int)
+    fun onSuccessPersonalResponse(response: PersonalUpdateStatusResponse)
+    fun onFailureResponse(error: Throwable)
+}
