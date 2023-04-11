bd7977c29c98f02a0e031e5fd901ded971e46094
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-04-11 02:19:48 +0200

    implementation  update personal status in Repository

--- app/src/main/java/com/example/busybee/data/Repository.kt
+++ app/src/main/java/com/example/busybee/data/Repository.kt
@@ -3,6 +3,7 @@ package com.example.busybee.data
 import android.content.Context
 import android.util.Base64
 import com.example.busybee.data.models.LoginResponse
+import com.example.busybee.data.models.PersonalUpdateStatusResponse
 import com.example.busybee.data.source.ConnectionBuilder
 import com.example.busybee.data.source.executeWithCallbacks
 import com.example.busybee.utils.AuthorizationInterceptor
@@ -46,5 +47,27 @@ class Repository(private val context: Context) : RepositoryInterface {
         )
 
     }
+
+    override fun <T> updateTasksPersonalStatus(
+        id: String,
+        status: Int,
+        onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit,
+    ) {
+        val request = Request.Builder()
+            .url(Constant.BASE_URL+Constant.PERSONAL_TASKS_END_POINT)
+            .build()
+
+        val responseType = object : TypeToken<PersonalUpdateStatusResponse>() {}.type
+
+        client.executeWithCallbacks(
+            request,
+            responseType,
+            onSuccessCallback,
+            onFailureCallback
+        )
+
+    }
+
 
 }
--- app/src/main/java/com/example/busybee/data/RepositoryInterface.kt
+++ app/src/main/java/com/example/busybee/data/RepositoryInterface.kt
@@ -1,7 +1,15 @@
 package com.example.busybee.data
 
 interface RepositoryInterface {
-    fun <T> logIn(userName: String, password: String, onSuccessCallback: (response: T) -> Unit,
-                  onFailureCallback: (error: Throwable) -> Unit )
+    fun <T> logIn(
+        userName: String, password: String, onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit,
+    )
+
+    fun <T> updateTasksPersonalStatus(
+        id: String, status: Int,
+        onSuccessCallback: (response: T) -> Unit,
+        onFailureCallback: (error: Throwable) -> Unit,
+    )
 
 }
