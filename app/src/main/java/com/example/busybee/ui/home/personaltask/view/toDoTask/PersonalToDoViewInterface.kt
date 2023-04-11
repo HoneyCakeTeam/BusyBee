c92ba32a349157a72d212fdab3d386f7a0ae0b05
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-11 12:39:48 +0200

    add PersonalToDoViewInterface

---
+++ app/src/main/java/com/example/busybee/ui/home/personaltask/view/toDoTask/PersonalToDoViewInterface.kt
@@ -0,0 +1,9 @@
+package com.example.busybee.ui.home.personaltask.view.toDoTask
+
+import com.example.busybee.data.models.PersonalCreateToDoResponse
+
+interface PersonalToDoViewInterface {
+    fun personalCreateToDo(title: String, description: String)
+    fun onSuccessResponse(response: PersonalCreateToDoResponse)
+    fun onFailureResponse(error: Throwable)
+}
