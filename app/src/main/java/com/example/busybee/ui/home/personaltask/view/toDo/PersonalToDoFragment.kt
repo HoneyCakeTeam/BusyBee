a1f3a685974046f4139bbf1cb3e07086e3620c69
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-11 06:06:30 +0200

    add newInstance function to PersonalToDoFragment.kt

--- app/src/main/java/com/example/busybee/ui/home/personaltask/view/toDo/PersonalToDoFragment.kt
+++ app/src/main/java/com/example/busybee/ui/home/personaltask/view/toDo/PersonalToDoFragment.kt
@@ -1,12 +1,18 @@
 package com.example.busybee.ui.home.personaltask.view.toDo
 
+import android.os.Bundle
 import com.example.busybee.base.BaseFragment
 import com.example.busybee.databinding.BottomSheetCreateTaskBinding
 import com.example.busybee.databinding.FragmentPersonalToDoBinding
+import com.example.busybee.domain.models.PersonalTodos
+import com.example.busybee.ui.home.personaltask.view.done.PersonalDoneFragment
+import com.example.busybee.ui.home.personaltask.view.inProgress.PersonalInProgressFragment
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class PersonalToDoFragment() : BaseFragment<FragmentPersonalToDoBinding>() {
     private lateinit var adapter: PersonalToDoAdapter
+    private lateinit var done: PersonalTodos
+
     override val TAG = this::class.java.simpleName.toString()
 
     override fun getViewBinding(): FragmentPersonalToDoBinding {
@@ -14,8 +20,9 @@ class PersonalToDoFragment() : BaseFragment<FragmentPersonalToDoBinding>() {
     }
 
     override fun setUp() {
+        getDons()
         addCallBacks()
-        adapter = PersonalToDoAdapter(emptyList())
+        adapter = PersonalToDoAdapter(done.values)
         binding.recyclerToDo.adapter = adapter
     }
 
@@ -36,6 +43,22 @@ class PersonalToDoFragment() : BaseFragment<FragmentPersonalToDoBinding>() {
         }
         bottomSheet.setContentView(binding.root)
         bottomSheet.show()
+    }
+
+    private fun getDons() {
+        arguments?.let {
+            done = it.getParcelable(PersonalToDoFragment.PERSONAL_TODO_LIST)!!
+        }
+    }
+
+    companion object {
+        const val PERSONAL_TODO_LIST = "Personal_Todo_List"
+        fun newInstance(tasks: PersonalTodos) =
+            PersonalToDoFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(PERSONAL_TODO_LIST, tasks)
+                }
+            }
     }
 
 }
