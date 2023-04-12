a13a0f0f6a775932a6c730f7443e566cd51bf6ae
Mohamed Naser <mahmmedn19@gmail.com>
2023-04-12 14:38:31 +0200

    update team response

--- app/src/main/java/com/example/busybee/ui/home/teamtask/view/todo/view/TeamToDoViewInterface.kt
+++ app/src/main/java/com/example/busybee/ui/home/teamtask/view/todo/view/TeamToDoViewInterface.kt
@@ -1,9 +1,10 @@
 package com.example.busybee.ui.home.teamtask.view.todo.view
 
+import com.example.busybee.data.models.TeamCreateToDoResponse
 import com.example.busybee.data.models.TeamToDoListResponse
 
 interface TeamToDoViewInterface {
     fun teamCreateToDo(title: String ,description: String, assignee: String)
-    fun onSuccessResponse(response: TeamToDoListResponse)
+    fun onSuccessResponse(response: TeamCreateToDoResponse)
     fun onFailureResponse(error: Throwable)
 }
