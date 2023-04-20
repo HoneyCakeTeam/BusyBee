acb3cd69c64a79edd3638ffdbd2300b7461ee079
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-04-20 04:17:43 +0200

    fix dependency inversion in MainActivity.kt

--- app/src/main/java/com/example/busybee/ui/MainActivity.kt
+++ app/src/main/java/com/example/busybee/ui/MainActivity.kt
@@ -1,28 +1,32 @@
 package com.example.busybee.ui
 
-import android.content.res.Configuration
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.busybee.R
 import com.example.busybee.data.RepositoryImp
+import com.example.busybee.data.source.RemoteDataSource
 import com.example.busybee.data.source.RemoteDataSourceImp
 import com.example.busybee.ui.home.HomeFragment
 import com.example.busybee.ui.login.LoginFragment
 import com.example.busybee.utils.Constant
+import com.example.busybee.utils.sharedpreference.SharedPreferencesInterface
 import com.example.busybee.utils.sharedpreference.SharedPreferencesUtils
-import java.util.*
 
 class MainActivity : AppCompatActivity(), MainView {
-
+    private val sharedPreferences: SharedPreferencesInterface by lazy {
+        SharedPreferencesUtils(
+            this
+        )
+    }
+    private val remoteDataSource: RemoteDataSource by lazy {
+        RemoteDataSourceImp(this)
+    }
     private val presenter by lazy {
         MainPresenter(
             RepositoryImp(
-                RemoteDataSourceImp(this),
-                SharedPreferencesUtils(this)
+                remoteDataSource,
+                sharedPreferences
             ), this
         )
     }
